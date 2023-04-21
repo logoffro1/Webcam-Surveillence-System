9de17ce4c13c17c7aa958462d0fec22a0a669db8
logoffro1 <cosmin.ilie2704@gmail.com>
2023-04-21 04:40:56 +0200

    yay

--- Frontend/websocket.js
+++ Frontend/websocket.js
@@ -1,44 +1,51 @@
-$(document).ready(function() {
+let password = prompt("Password");
+if(password == "bigchongus"){
 
-	var ws = new WebSocket("ws://d8ba-80-114-172-251.ngrok-free.app");
+
+$(document).ready(function() {
+	var ws = new WebSocket("ws://b821-80-114-172-251.ngrok-free.app");
 	//var ws = new WebSocket("ws://127.0.0.1:8123");
 	ws.onopen = function(e) {
 		if (typeof console !== 'undefined') {
 			console.info('WS open');
 		}
-		alert("test");
-
 	};
 
 	ws.onmessage = function (e) {
-		
+		var d = new Date();
 		var data = JSON.parse(e.data),
 			type = data.type,
 			i = 0,
 			$webcams = $('#webcams'),
 			$img = null;
+			$fps = 0;
+
+		 $fps = $("<h3>FPS: "+data.fps+"</h3>");
 		if (typeof console !== 'undefined') {
 			console.info('WS message', type);
 		}
-
 		if (type === 'list') {
 			for (i = 0; i < data.webcams.length; i += 1) {
 				$img = $("<img></img>")
 					.attr("src", "webcam-capture-logo-small.jpg")
 					.attr("alt", data.webcams[i])
 					.attr("name", data.webcams[i]);
-				$webcams.append($img)
+					$webcams.append($date)
+					$webcams.append($fps)
+					$webcams.append($img)
+				
 			}
 		} else if (type === 'image') {
+			var d = new Date(Date.now());
+			var $date = d.getHours() + ":" + d.getMinutes() + ":"+d.getSeconds();
+			var $fps = $("h3").html("Time: " + $date +" | FPS: "+data.fps);
+			
+
 			var $img = $("img[name='" + data.webcam + "']")
 				.attr("src", "data:image/jpeg;base64," + data.image)
 				.addClass('shadow')
 				.trigger("change");
-			setTimeout(function() {
-				$img
-					.removeClass('shadow')
-					.trigger("change");
-			}, 1000);
+
 		}
 	};
 
@@ -54,3 +61,4 @@ $(document).ready(function() {
 		}
 	};
 });
+}
